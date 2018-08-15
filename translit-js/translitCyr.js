56c0c5f21eb2bcd490780bdb5119b778c0bb4d0d
Alexandr <alexmar12@yandex.ru>
2018-08-15 10:07:11 +0300

    Update translitCyr.js

--- translit-js/translitCyr.js
+++ translit-js/translitCyr.js
@@ -1,98 +1,116 @@
-﻿function cyrillicToTranslit(config) {
-  const _preset = config ? config.preset : "ru";
-
-  const _firstLetterAssociations = {
-    "а": "a",
-    "б": "b",
-    "в": "v",
-    "ґ": "g",
-    "г": "g",
-    "д": "d",
-    "е": "e",
-    "ё": "e",
-    "є": "ye",
-    "ж": "zh",
-    "з": "z",
-    "и": "i",
-    "і": "i",
-    "ї": "yi",
-    "й": "i",
-    "к": "k",
-    "л": "l",
-    "м": "m",
-    "н": "n",
-    "о": "o",
-    "п": "p",
-    "р": "r",
-    "с": "s",
-    "т": "t",
-    "у": "u",
-    "ф": "f",
-    "х": "h",
-    "ц": "c",
-    "ч": "ch",
-    "ш": "sh",
-    "щ": "sh'",
-    "ъ": "",
-    "ы": "i",
-    "ь": "",
-    "э": "e",
-    "ю": "yu",
-    "я": "ya",
-  };
-
-  if (_preset === "uk") {
-    Object.assign(_firstLetterAssociations, {
-      "г": "h",
-      "и": "y",
-      "й": "y",
-      "х": "kh",
-      "ц": "ts",
-      "щ": "shch",
-      "'": "",
-      "’": "",
-      "ʼ": "",
+$(window).on('load', function(event) {
+    $('.input-in').keyup(function() {
+        $('.input-out').val(cyrillicToTranslit().transform($(this).val()));
     });
-  }
+});
 
-  const _associations = Object.assign({}, _firstLetterAssociations);
 
-  if (_preset === "uk") {
-    Object.assign(_associations, {
-      "є": "ie",
-      "ї": "i",
-      "й": "i",
-      "ю": "iu",
-      "я": "ia",
-    });
-  }
+function cyrillicToTranslit(config) {
+    const _preset = config ? config.preset : "ru";
 
-  function transform(input, spaceReplacement) {
-    if (!input) {
-      return "";
+    const _firstLetterAssociations = {
+        "а": "a",
+        "б": "b",
+        "в": "v",
+        "ґ": "g",
+        "г": "g",
+        "д": "d",
+        "е": "e",
+        "ё": "e",
+        "є": "ye",
+        "ж": "zh",
+        "з": "z",
+        "и": "i",
+        "і": "i",
+        "ї": "yi",
+        "й": "i",
+        "к": "k",
+        "л": "l",
+        "м": "m",
+        "н": "n",
+        "о": "o",
+        "п": "p",
+        "р": "r",
+        "с": "s",
+        "т": "t",
+        "у": "u",
+        "ф": "f",
+        "х": "h",
+        "ц": "c",
+        "ч": "ch",
+        "ш": "sh",
+        "щ": "sh'",
+        "ъ": "",
+        "ы": "i",
+        "?": "",
+        "ь": "",
+        "э": "e",
+        "ю": "yu",
+        "я": "ya",
+        " ": "-",
+        ".": "",
+        "(": "",
+        ")": "",
+        ",": "",
+        "'": "",
+        "’": "",
+        "'": "",
+        '"': "",
+        "«": "",
+        "»": "",
+    };
+
+    if (_preset === "uk") {
+        Object.assign(_firstLetterAssociations, {
+            "г": "h",
+            "и": "y",
+            "й": "y",
+            "х": "kh",
+            "ц": "ts",
+            "щ": "shch",
+            "'": "",
+            "’": "",
+            "ʼ": "",
+        });
     }
 
-    let newStr = "";
-    for (let i = 0; i < input.length; i++) {
-      let strLowerCase = input[i].toLowerCase();
-      if (strLowerCase === " " && spaceReplacement) {
-        newStr += spaceReplacement;
-        continue;
-      }
-      let newLetter = _preset === "uk" && strLowerCase === "г" && i > 0 && input[i - 1].toLowerCase() === "з"
-        ? "gh"
-        : (i === 0 ? _firstLetterAssociations : _associations)[strLowerCase];
-      if ("undefined" === typeof newLetter) {
-        newStr += strLowerCase;
-      }
-      else {
-        newStr += newLetter;
-      }
-    }
-    return newStr;
-  }
+    const _associations = Object.assign({}, _firstLetterAssociations);
 
-  return {
-    transform: transform
-  };
+    if (_preset === "uk") {
+        Object.assign(_associations, {
+            "є": "ie",
+            "ї": "i",
+            "й": "i",
+            "ю": "iu",
+            "я": "ia",
+        });
+    }
+
+    function transform(input, spaceReplacement) {
+        if (!input) {
+            return "";
+        }
+
+        let newStr = "";
+        for (let i = 0; i < input.length; i++) {
+            let strLowerCase = input[i].toLowerCase();
+            if (strLowerCase === " " && spaceReplacement) {
+                newStr += spaceReplacement;
+                continue;
+            }
+            let newLetter = _preset === "uk" && strLowerCase === "г" && i > 0 && input[i - 1].toLowerCase() === "з" ?
+                "gh" :
+                (i === 0 ? _firstLetterAssociations : _associations)[strLowerCase];
+            if ("undefined" === typeof newLetter) {
+                newStr += strLowerCase;
+            } else {
+                newStr += newLetter;
+            }
+        }
+        return newStr;
+    }
+
+    return {
+        transform: transform
+    };
 };
